f1b5eff426cb3db04bbb78879d14bf4f7d399a6a
nahin88 <nahiraja88@gmail.com>
2023-03-20 12:03:33 +0600

    countries and flags added

--- js/countries.js
+++ js/countries.js
@@ -1,1 +1,25 @@
-console.log('countries.js');
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => displayCountries(data))
+};
+
+const displayCountries = countries => {
+    console.log(countries[0]);
+    const countryHtml = countries.map(country => getCountryHtml(country));
+    // console.log(countryHtml[0]);
+    const container = document.getElementById('countries');
+    container.innerHTML = countryHtml.join(' ');
+}
+
+const getCountryHtml = country => {
+    return `
+        <div class="single-country">
+            <h2>${country.name.common}</h2>
+            <img src="${country.flags.png}">
+        </div>
+    `
+}
+
+
+loadCountries()
