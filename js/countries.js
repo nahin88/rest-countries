82b34634de423af406abb0a7c8687f4af748f919
nahin88 <nahiraja88@gmail.com>
2023-03-20 19:52:42 +0600

    destructuring added

--- js/countries.js
+++ js/countries.js
@@ -11,15 +11,45 @@ const displayCountries = countries => {
     const container = document.getElementById('countries');
     container.innerHTML = countryHtml.join(' ');
 }
+//original
+// const getCountryHtml = country => {
+//     return `
+//         <div class="single-country">
+//             <h2>${country.name.common}</h2>
+//             <img src="${country.flags.png}">
+            
+//                 <h3>Population: ${country.population}</h3>
+//                 <p>${country.capital?country.capital:'no data found'}</p>
+//         </div>
+//     `
+// }
 
-const getCountryHtml = country => {
+//option 1
+// const getCountryHtml = country => {
+//     const { name, flags, capital, population } = country;
+//     return `
+//     <div class="single-country">
+//         <h2>${name.common}</h2>
+//         <img src="${flags.png}">
+        
+//             <h3>Population: ${population}</h3>
+//             <p>${capital ? capital : 'no data found'}</p>
+//     </div>
+// `
+// }
+
+//option 2
+const getCountryHtml = ({ name, flags, capital, population,area,region }) => {
     return `
-        <div class="single-country">
-            <h2>${country.name.common}</h2>
-            <img src="${country.flags.png}">
-        </div>
-    `
+    <div class="single-country">
+        <h2>${name.common}</h2>
+        <p style="color:red;">${area}</p>
+        <h3 style="color:green;">${region}</h3>
+        <img src="${flags.png}">
+        <h3>Population: ${population}</h3>
+        <p>${capital ? capital : 'no data found'}</p>
+    </div>
+`
 }
-
 
 loadCountries()
